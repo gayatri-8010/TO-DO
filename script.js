00e53a24b073d9dc2339103c47c70c8851c182e0
gayatri-8010 <gayatrithorat03@gmail.com>
2025-06-16 11:39:16 +0530

    added to do code

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
+const todoInput = document.getElementById("todo-input");
+const todosParent = document.getElementById("todos");
+
 function addTodo() {
-      const input = document.getElementById('todo-input');
-      const text = input.value;
+  const div = document.createElement("div");
+  const para = document.createElement("p");
+  const button = document.createElement("button");
+  const button2=document.createElement("button");
 
-      const todoDiv = document.createElement('div');
-      todoDiv.className = "todo-item";
+  const todoId= Date.now();
+ 
+  div.id=todoId
+  button.setAttribute("onclick",`deleteTodo(${todoId})`);
+  button.style.color("red");
+  button2.setAttribute("onclick",`completed(${todoId})`);
+  button.innerText = "Delete";
+  para.innerText = todoInput.value;
+  button2.innerText="completed";
 
-      const para = document.createElement('p');
-      para.textContent = text;
-      
+  div.append(para, button,button2);
+  todosParent.appendChild(div);
+}
+  function deleteTodo(todoId){
+    const el=document.getElementById(todoId);
+    el.remove();
+  }
 
-      const deletebutton=document.createElement('button');
-      deletebutton.innerHTML="Delete";
-      deletebutton.className="deletebtn";
-      
-      deletebutton.onclick=function(){
-        todoDiv.remove();
-      };
-      
-      todoDiv.appendChild(para);
-      todoDiv.appendChild(deletebutton);
-
-
-     document.getElementById('todo-list').appendChild(todoDiv);
-
-      input.value="";
-       }
+  function completed(todoId){
+  const el2=document.getElementById(todoId);
+  el2.classList.toggle("complete");
+  }
